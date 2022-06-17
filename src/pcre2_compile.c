0eb7a1c11abb298a778c38394ad20f4e2865ed57
Arnoud Zwemmer <arnoud@airtechnology.com>
2022-06-17 11:10:16 +0700

    Enforce not linking to unused jit_free_8()

--- src/pcre2_compile.c
+++ src/pcre2_compile.c
@@ -1266,9 +1266,10 @@ PCRE2_SIZE* ref_count;
 
 if (code != NULL)
   {
+#ifdef SUPPORT_JIT
   if (code->executable_jit != NULL)
     PRIV(jit_free)(code->executable_jit, &code->memctl);
-
+#endif
   if ((code->flags & PCRE2_DEREF_TABLES) != 0)
     {
     /* Decoded tables belong to the codes after deserialization, and they must
